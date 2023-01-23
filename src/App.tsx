5b3e812512d71880e213af5e8f603c952d9392a2
SergBily <89011743+SergBily@users.noreply.github.com>
2023-01-23 22:21:24 +0300

    refactor: change settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.scss';
-import TableData from './components/table/TableData';
+import TableData from './components/TableData';
+import ToolbarApp from './components/ToolbarApp';
 
 const App = () => (
   <div className="App">
+    <ToolbarApp />
     <TableData />
-    <p>hello</p>
   </div>
 );
 
--- src/components/toolbar/Toolbar.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './toolbar.scss';
-
-const Toolbar = () => (<div>hello</div>);
-
-export default Toolbar;
