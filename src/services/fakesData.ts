a45f2c6704d0e8e1188a1a0cb0049dc81720098a
SergBily <89011743+SergBily@users.noreply.github.com>
2023-01-27 04:49:44 +0300

    fix: update request to server

--- src/services/fakesData.ts
+++ src/services/fakesData.ts
@@ -5,13 +5,13 @@ import { ResponseRandomData } from '../models/responseRandomData';
 
 export default class RandomData {
   static async getRandomData(p: RequestRandomData): Promise<ResponseRandomData[]> {
-    const response = await axios.post('http://localhost:5000/main', p);
+    const response = await axios.post('https://task5-server-itransition.herokuapp.com/main', p);
     const { data } = response;
     return data;
   }
 
   static async getCsvFile(): Promise<void> {
-    const response = await axios.get('http://localhost:5000/export', { responseType: 'blob' });
+    const response = await axios.get('https://task5-server-itransition.herokuapp.com/export', { responseType: 'blob' });
     const { data } = response;
     fileDownload(data, 'export-file.csv');
   }
